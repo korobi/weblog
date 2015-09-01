edea4da2db4a17669f2d68324d970e65a0c68df6
bendem <online@bendem.be>
2015-09-01 23:15:47 +0200

    Theme switcher

---
+++ js/main.js
@@ -0,0 +1,45 @@
+(function() {
+    'use strict';
+
+    var theme = localStorage['theme'] === 'light' ? 'light' : 'dark';
+    var $html;
+    var $switcher;
+
+    if(document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', init);
+    } else {
+        init();
+    }
+
+    function init() {
+        $html = document.querySelector('html');
+        $switcher = document.querySelector('#theme-switcher');
+
+        $switcher.addEventListener('click', function(e) {
+            localStorage['theme'] = theme = switchTheme(invert(theme));
+        });
+
+        if(theme === 'light') {
+            switchTheme(theme);
+        }
+    }
+
+    function switchTheme(theme) {
+        if(theme === 'light') {
+            $html.classList.add('light');
+            $switcher.classList.add('theme-switcher--to-dark');
+            $switcher.classList.remove('theme-switcher--to-light');
+            $switcher.querySelector('.theme-switcher--js-type').textContent = 'dark';
+        } else {
+            $html.classList.remove('light');
+            $switcher.classList.remove('theme-switcher--to-dark');
+            $switcher.classList.add('theme-switcher--to-light');
+            $switcher.querySelector('.theme-switcher--js-type').textContent = 'light';
+        }
+        return theme;
+    }
+
+    function invert(theme) {
+        return theme === 'light' ? 'dark' : 'light';
+    }
+})();
